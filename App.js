90bdb07cd4e51447a7450a84c840b978445e69df
Fx64b <90456762+Fx64b@users.noreply.github.com>
2024-02-05 11:13:25 +0100

    Add README + cleanup

--- App.js
+++ App.js
@@ -1,20 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View, Pressable} from 'react-native';
+import React, {useEffect, useState} from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [milliseconds, setMilliseconds] = useState(0);
+    const [running, setRunning] = useState(false);
+    const [title, setTitle] = useState('START')
+    let interval;
+
+    useEffect(() => {
+        if (running) {
+            interval = setInterval(() => {
+                setMilliseconds(prevMilliseconds => prevMilliseconds + 10);
+            }, 10);
+        }
+
+        return () => clearInterval(interval);
+    }, [running]);
+
+    const startStopwatch = () => {
+        setRunning(true);
+        setTitle('STOP')
+    };
+
+    const stopStopwatch = () => {
+        clearInterval(interval);
+        setTitle('CONTINUE')
+        setRunning(false);
+    };
+
+    const resetStopwatch = () => {
+        clearInterval(interval);
+        setTitle('START')
+        setMilliseconds(0);
+        setRunning(false);
+    };
+
+    const formatTime = () => {
+        const getMilliseconds = (`00${milliseconds % 1000}`).slice(-3);
+        const seconds = Math.floor(milliseconds / 1000);
+        const getSeconds = (`0${seconds % 60}`).slice(-2);
+        const minutes = Math.floor(seconds / 60);
+        const getMinutes = (`0${minutes % 60}`).slice(-2);
+        const getHours = (`0${Math.floor(minutes / 60)}`).slice(-2);
+
+        return `${getHours}:${getMinutes}:${getSeconds}.${getMilliseconds}`;
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.timer}>{formatTime()}</Text>
+            <View style={styles.buttons}>
+                <Pressable style={styles.button} onPress={running ? stopStopwatch : startStopwatch}>
+                    <Text style={styles.text}>{title}</Text>
+                </Pressable>
+                <Pressable style={styles.button} onPress={resetStopwatch}>
+                    <Text style={styles.text}>RESET</Text>
+                </Pressable>
+            </View>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#111111',
+    },
+    timer: {
+        fontSize: 60,
+        marginBottom: 40,
+        color: 'white'
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '80%',
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 18,
+        borderRadius: 8,
+        elevation: 3,
+        backgroundColor: '#404040',
+        cursor: 'pointer',
+        width: '40%'
+    },
+    text: {
+        fontSize: 20,
+        lineHeight: 26,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'white',
+    },
 });
---
+++ README.md
@@ -0,0 +1,27 @@
+# Stopwatch ÜK M335
+
+An example stopwatch project created in the ÜK M335.
+
+https://www.modulbaukasten.ch/module/335/3/de-DE?title=Mobile-Applikation-realisieren
+
+<br>
+
+## Usage
+
+
+**Installation**
+
+```bash
+npm i
+```
+
+**Run on android**
+
+```bash
+npm run android
+```
+
+**Run on web**
+```bash
+npm run web
+```
